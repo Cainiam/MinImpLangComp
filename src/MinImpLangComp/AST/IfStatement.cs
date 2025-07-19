bc9e0db73506edf3a1c9e08734ad49b569c14167
Jordan Boisdenghien <Cainiam@users.noreply.github.com>
2025-07-19 21:41:00 +0200

    Add if, while and forStatement classes in AST
    - IfStatement.cs for if-else statements for in AST
    - WhileStatement for while loops in AST
    - ForStatement for for loops in ASR

---
+++ src/MinImpLangComp/AST/IfStatement.cs
@@ -0,0 +1,16 @@
+﻿namespace MinImpLangComp.AST
+{
+    public class IfStatement : Statement
+    {
+        public Expression Condition { get; }
+        public Statement ThenBranch { get; }
+        public Statement? ElseBranch { get; }
+
+        public IfStatement(Expression condition, Statement thenBranch, Statement? elseBranch = null)
+        {
+            Condition = condition;
+            ThenBranch = thenBranch;
+            ElseBranch = elseBranch;
+        }
+    }
+}
---
+++ src/MinImpLangComp/AST/WhileStatement.cs
@@ -0,0 +1,14 @@
+﻿namespace MinImpLangComp.AST
+{
+    public class WhileStatement : Statement
+    {
+        public Expression Condition { get; }
+        public Statement Body { get; }
+
+        public WhileStatement(Expression condition, Statement body)
+        {
+            Condition = condition;
+            Body = body;
+        }
+    }
+}
