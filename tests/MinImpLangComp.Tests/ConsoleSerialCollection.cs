a5f998f5a54868de17d112902261267e18cd2a3e
Jordan Boisdenghien <Cainiam@users.noreply.github.com>
2025-08-18 12:02:39 +0200

    Document ConsoleSerial collection
    - Add XML documentation
    - Delete useless using
    - No behavior changes.

--- tests/MinImpLangComp.Tests/ConsoleSerialCollection.cs
+++ tests/MinImpLangComp.Tests/ConsoleSerialCollection.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit;
 
 namespace MinImpLangComp.Tests
 {
+    /// <summary>
+    /// xUnit test collection used to serialize tests that interact with console I/O
+    /// <para>
+    /// By disabling parallelization, we avoid contention on global state such as <see cref="System.Console"/>, environnement variables, and thread-local buffers.
+    /// </para>
+    /// </summary>
     [CollectionDefinition("ConsoleSerial", DisableParallelization = true)]
     public class ConsoleSerialCollection : ICollectionFixture<object> 
     { 
-        //
+        // No Code needed.
     }
 }
